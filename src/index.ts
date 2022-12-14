bfb3a5f2c223ed80ba8b74a809b574490dff32da
dain <57316771+DainPark-web@users.noreply.github.com>
2022-12-14 22:08:57 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log("hey");
+import crypto from "crypto";
+
+
+
+interface BlockShape {
+  prevHash: string;
+  height: number;
+  data: string;
+  hash: string;
+}
+
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ){
+    this.hash = Block.calculatedHash(prevHash, height, data);
+  }
+
+  static calculatedHash(prevHash: string, height: number, data:string):string{
+    const toHash = `${prevHash}${height}${data}`
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
+
+
+
